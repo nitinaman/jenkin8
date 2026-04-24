a5325e546d173fc3ac9393be89399214a5b88dbc
nitinaman <nitinaman3@gmail.com>
2026-04-24 12:45:39 +0530

    Update App.java

--- App.java
+++ App.java
@@ -15,7 +15,7 @@ public class App {
     }
     public static void main(String[] args) {
         App c = new App();
-        System.out.println("Addition" + c.add(10, 5));
+        System.out.println("Addition" + c.add(150, 5));
         System.out.println("Subtraction" + c.sub(10, 5));
         System.out.println("Multiplication" + c.mul(10, 5));
         System.out.println("Division" + c.div(10, 5));
