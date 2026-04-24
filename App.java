9d806e7287e5c7a6903d7a8de7f71d9e41144b45
nitinaman <nitinaman3@gmail.com>
2026-04-24 12:43:16 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,23 @@
+public class App {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int sub(int a, int b) {
+        return a - b;
+    }
+    public int mul(int a, int b) {
+        return a * b;
+    }
+    public int div(int a, int b) {
+        if (b == 0)
+            throw new ArithmeticException("Cannot divide by zero");
+        return a / b;
+    }
+    public static void main(String[] args) {
+        calculator c = new calculator();
+        System.out.println("Addition" + c.add(10, 5));
+        System.out.println("Subtraction" + c.sub(10, 5));
+        System.out.println("Multiplication" + c.mul(10, 5));
+        System.out.println("Division" + c.div(10, 5));
+    }
+}
